b49feb21ebb8b286c902e76f6ad9855d69e09266
Vesely-jiri <vesely.jiri1998@gmail.com>
2025-04-30 11:14:13 +0200

    Recreated application profiles, added pom.xml dependencies for basic SpringBoot application

--- src/main/java/cz/jpcz/Main.java
+++ src/main/java/cz/jpcz/Main.java
@@ -1,7 +1,13 @@
 package cz.jpcz;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Executing program in last portfolio project: Genesis resources");
+
+        SpringApplication.run(Main.class, args);
     }
 }
